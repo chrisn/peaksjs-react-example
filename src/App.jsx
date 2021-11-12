7c7fe459214cdbc0116e889ff0f5b3f3a5be80e9
Adolfo Vazquez <adolfo.software@gmail.com>
2021-11-11 18:11:08 -0600

    Add id property to ToggleButton elements

--- src/App.jsx
+++ src/App.jsx
@@ -60,8 +60,8 @@ class App extends Component {
               name="options"
               defaultValue={1}
               onChange={this.handleSelectedAudioChange}>
-              <ToggleButton value={1}>Bird song</ToggleButton>&nbsp;
-              <ToggleButton value={2}>Car passing</ToggleButton>
+              <ToggleButton id={1} value={1}>Bird song</ToggleButton>
+              <ToggleButton id={2} value={2}>Car passing</ToggleButton>
             </ToggleButtonGroup>
           </ButtonToolbar>
 
